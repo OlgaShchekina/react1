bcf3042280a56b3d5261a7d278c319c50cb01479
olgashchekina <olya6avg@gmail.com>
2020-02-22 11:21:38 -0800

    Updates

---
+++ precache-manifest.af94834d78d525d7e692d117fc69daaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750e9d0d4a8b821e13ce1b2d0be2d20d",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "ec55cf586f989fcd2858",
+    "url": "/react1/static/css/main.09be938b.chunk.css"
+  },
+  {
+    "revision": "88559eab60097191c142",
+    "url": "/react1/static/js/2.be7b4471.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react1/static/js/2.be7b4471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec55cf586f989fcd2858",
+    "url": "/react1/static/js/main.b5f9cab3.chunk.js"
+  },
+  {
+    "revision": "8458cd5984e33d39edd8",
+    "url": "/react1/static/js/runtime-main.8ad5742a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react1/static/media/logo.5d5d9eef.svg"
+  }
+]);
